17a5d6339d9cdc49977e2dd045ab2576a8108e42
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-03-24 19:36:16 +0700

    update

--- Cau2_FavoriteMusicList/app/src/main/java/ntu/nmh61133637/cau2_favoritemusiclist/MainActivity.java
+++ Cau2_FavoriteMusicList/app/src/main/java/ntu/nmh61133637/cau2_favoritemusiclist/MainActivity.java
@@ -3,12 +3,32 @@ package ntu.nmh61133637.cau2_favoritemusiclist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<MusicList> musicLists;
+    EditText txtSong, txtSinger;
+    Button btnSong;
+    ListView listView;
+    int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void findView() {
+        txtSong = findViewById(R.id.txtAddSong);
+        txtSinger = findViewById(R.id.txtAddSinger);
+        btnSong = findViewById(R.id.btnSong);
+        listView = findViewById(R.id.list_music);
+    }
+
+    public void songHandle(View view) {
     }
 }
