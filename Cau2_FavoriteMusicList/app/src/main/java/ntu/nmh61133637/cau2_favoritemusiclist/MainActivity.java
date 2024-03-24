4b9b692f607f6a1b5f71a087f64940bdcff16a3c
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-03-24 17:48:59 +0700

    Cau2_ThiGiuaKy
    Cau2_ThiGiuaKy

---
+++ Cau2_FavoriteMusicList/app/src/main/java/ntu/nmh61133637/cau2_favoritemusiclist/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.nmh61133637.cau2_favoritemusiclist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
