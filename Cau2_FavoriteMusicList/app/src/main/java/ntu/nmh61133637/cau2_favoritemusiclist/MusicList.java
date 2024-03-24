0cdaa1cbf1b6945a1c7302ef9183a4795b4edf4a
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-03-24 18:50:13 +0700

    Update

---
+++ Cau2_FavoriteMusicList/app/src/main/java/ntu/nmh61133637/cau2_favoritemusiclist/MusicList.java
@@ -0,0 +1,8 @@
+package ntu.nmh61133637.cau2_favoritemusiclist;
+
+public class MusicList {
+    String Name, Singer;
+    public MusicList(String name, String singer) {
+        this.Name = name; this.Singer = singer;
+    }
+}
