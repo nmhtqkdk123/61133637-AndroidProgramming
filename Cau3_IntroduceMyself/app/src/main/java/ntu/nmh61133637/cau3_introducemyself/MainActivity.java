32e13eb46b38c411a8601c45a5bd5b7caa543316
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-03-26 00:28:20 +0700

    update

--- Cau3_IntroduceMyself/app/src/main/java/ntu/nmh61133637/cau3_introducemyself/MainActivity.java
+++ Cau3_IntroduceMyself/app/src/main/java/ntu/nmh61133637/cau3_introducemyself/MainActivity.java
@@ -3,6 +3,8 @@ package ntu.nmh61133637.cau3_introducemyself;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +14,14 @@ public class MainActivity extends AppCompatActivity {
     EditText txtIdVal;
     EditText txtPhoneVal;
     EditText txtEmailVal;
+    Button btnEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findItem();
+        getInfo();
     }
     private void findItem() {
         txtNameVal = findViewById(R.id.txtNameValue);
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         txtIdVal = findViewById(R.id.txtIDVal);
         txtPhoneVal = findViewById(R.id.txtPhoneVal);
         txtEmailVal = findViewById(R.id.txtEmailVal);
+        btnEdit = findViewById(R.id.btnEdit);
     }
     private void getInfo() {
         String name = txtNameVal.getText().toString();
@@ -33,5 +38,25 @@ public class MainActivity extends AppCompatActivity {
         String phone = txtPhoneVal.getText().toString();
         String email = txtEmailVal.getText().toString();
         info = new Info(name, Class, id, phone, email);
+    }
+
+    private void editInfo() {
+        setEditEnable(true);
+        btnEdit.setText("Đồng ý");
+    }
+    public void editHandle(View view) {
+        if(btnEdit.getText().equals("Chỉnh sửa thông tin")) editInfo();
+        else {
+            btnEdit.setText("Chỉnh sửa thông tin");
+            setEditEnable(false);
+            getInfo();
+        }
+    }
+    public void setEditEnable(boolean check) {
+        txtNameVal.setEnabled(check);
+        txtClassVal.setEnabled(check);
+        txtIdVal.setEnabled(check);
+        txtPhoneVal.setEnabled(check);
+        txtEmailVal.setEnabled(check);
     }
 }
