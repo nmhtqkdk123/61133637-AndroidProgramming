41bdfdec7ed3de94be20429a864393ba7f82d2a1
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-03-26 00:11:52 +0700

    update

---
+++ Cau3_IntroduceMyself/app/src/main/java/ntu/nmh61133637/cau3_introducemyself/Info.java
@@ -0,0 +1,8 @@
+package ntu.nmh61133637.cau3_introducemyself;
+
+public class Info {
+    String Name, Class, Id, Phone, Email;
+    public Info(String name, String Class, String id, String phone, String email) {
+        this.Name = name; this.Class = Class; this.Id = id; this.Phone = phone; this.Email = email;
+    }
+}
--- Cau3_IntroduceMyself/app/src/main/java/ntu/nmh61133637/cau3_introducemyself/MainActivity.java
+++ Cau3_IntroduceMyself/app/src/main/java/ntu/nmh61133637/cau3_introducemyself/MainActivity.java
@@ -3,12 +3,35 @@ package ntu.nmh61133637.cau3_introducemyself;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Info info;
+    EditText txtNameVal;
+    EditText txtClassVal;
+    EditText txtIdVal;
+    EditText txtPhoneVal;
+    EditText txtEmailVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findItem();
+    }
+    private void findItem() {
+        txtNameVal = findViewById(R.id.txtNameValue);
+        txtClassVal = findViewById(R.id.txtClassVal);
+        txtIdVal = findViewById(R.id.txtIDVal);
+        txtPhoneVal = findViewById(R.id.txtPhoneVal);
+        txtEmailVal = findViewById(R.id.txtEmailVal);
+    }
+    private void getInfo() {
+        String name = txtNameVal.getText().toString();
+        String Class = txtClassVal.getText().toString();
+        String id = txtIdVal.getText().toString();
+        String phone = txtPhoneVal.getText().toString();
+        String email = txtEmailVal.getText().toString();
+        info = new Info(name, Class, id, phone, email);
     }
 }
