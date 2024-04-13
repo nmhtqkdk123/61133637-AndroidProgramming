a68162bb00ca4eab4d2132bc741db65f4ad23bec
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-04-13 15:30:03 +0700

    Create PlacesListAdapter.java

---
+++ RecyclerView/app/src/main/java/ntu/nmh61133637/recyclerview/PlacesListAdapter.java
@@ -0,0 +1,59 @@
+package ntu.nmh61133637.recyclerview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class PlacesListAdapter extends RecyclerView.Adapter<PlacesListAdapter.PlaceItemHolder> {
+    Context context;
+    ArrayList<PlacesList> placesList;
+
+    public PlacesListAdapter(Context context, ArrayList<PlacesList> placesList) {
+        this.context = context;
+        this.placesList = placesList;
+    }
+
+    @NonNull
+    @Override
+    public PlaceItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater li = LayoutInflater.from(context);
+        View viewItem = li.inflate(R.layout.place_layout, parent, false);
+        PlaceItemHolder item = new PlaceItemHolder(viewItem);
+        return item;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaceItemHolder holder, int position) {
+        PlacesList places_list = placesList.get(position);
+        holder.placeName.setText(places_list.placeName);
+        String packageName = holder.itemView.getContext().getPackageName();
+        @SuppressLint("DiscouragedApi") int imageID = holder.itemView.getResources().getIdentifier(places_list.placeImageName, "mipmap", packageName);
+        holder.placeImage.setImageResource(imageID);
+    }
+
+    @Override
+    public int getItemCount() {
+        return placesList.size();
+    }
+
+    static final class PlaceItemHolder extends RecyclerView.ViewHolder {
+        TextView placeName;
+        ImageView placeImage;
+
+        public PlaceItemHolder(@NonNull View itemView) {
+            super(itemView);
+            placeName = itemView.findViewById(R.id.placeName);
+            placeImage = itemView.findViewById(R.id.placeImage);
+
+        }
+    }
+}
