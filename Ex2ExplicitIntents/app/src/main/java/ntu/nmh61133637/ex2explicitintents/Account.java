7c9a5d6cd540f9dfd8b6f4c41910e8f558db0d5a
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-04-24 14:06:29 +0700

    Ex2 Explicit Intents

---
+++ Ex2ExplicitIntents/app/src/main/java/ntu/nmh61133637/ex2explicitintents/Account.java
@@ -0,0 +1,30 @@
+package ntu.nmh61133637.ex2explicitintents;
+
+public class Account {
+    String username = "ngominhhieu";
+    String password = "61133637";
+    String email = "hieu.nmi.61cntt@ntu.edu.vn";
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ Ex2ExplicitIntents/app/src/main/java/ntu/nmh61133637/ex2explicitintents/HomeActivity.java
@@ -0,0 +1,14 @@
+package ntu.nmh61133637.ex2explicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
