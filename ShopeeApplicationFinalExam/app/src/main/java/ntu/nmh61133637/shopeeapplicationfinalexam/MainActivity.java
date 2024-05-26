0dd1a2fe63ed0beab5912d53e5bdd6f085b68b11
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-05-26 16:19:40 +0700

    update app

--- ShopeeApplicationFinalExam/app/src/main/java/ntu/nmh61133637/shopeeapplicationfinalexam/MainActivity.java
+++ ShopeeApplicationFinalExam/app/src/main/java/ntu/nmh61133637/shopeeapplicationfinalexam/MainActivity.java
@@ -3,6 +3,10 @@ package ntu.nmh61133637.shopeeapplicationfinalexam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createDatabase(View view) {
+        try (tb_AccountHandler tb_account = new tb_AccountHandler(MainActivity.this)) {
+            List<Account> accounts = tb_account.getAllAccount();
+            for(Account account: accounts) {
+                Log.d(String.valueOf(account.id), account.name);
+            }
+        } catch (Exception e) {System.out.println(e.getMessage());}
     }
 }
